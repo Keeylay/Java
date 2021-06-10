584538fdfa4fd58738f3f5d65a0ea3ae0e1089a5
Keeylay <kyrutkowski@gmail.com>
2021-06-10 14:53:40 -0500

    first programing stuff java

---
+++ Fundamentals/javafun/FirstJavaProgram.java
@@ -0,0 +1,5 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is Keeylay, I am 50 years old and my hometown is Chicago, IL");
+    }
+}
